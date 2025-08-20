7f597c6df0e88d85ab92d99534480f8109c52e53
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2025-08-21 01:21:21 +0530

    added sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import assets, { userDummyData } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
+  const navigate = useNavigate();
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div
+      className={` bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-orange-100 ${
+        selectedUser ? "max-md:hidden" : ""
+      }`}
+    >
+      <div className="pb-5">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo2} alt="Logo" className="max-w-40" />
+          <div className="relative py-2 group">
+            <img
+              src={assets.menu_icon}
+              alt="Menu"
+              className="max-h-5 cursor-pointer"
+            />
+            <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#cd3116] border border-orange-600 text-orange-100 hidden group-hover:block">
+              <p
+                onClick={() => navigate("/profile")}
+                className="cursor-pointer text-sm"
+              >
+                Edit Profile
+              </p>
+              <hr className="my-2 border-t border-orange-500" />
+              <p className="cursor-pointer text-sm">Logout</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#cd3116] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
+          <img src={assets.search_icon} alt="Search" className="w-3" />
+          <input
+            type="text"
+            placeholder="Search User..."
+            className="bg-transparent border-none outline-none text-orange-100 text-xs placeholder-[#e9dbdb] flex-1"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        {userDummyData.map((user, index) => (
+          <div
+            key={index}
+            onClick={() => setSelectedUser(user)}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user._id && "bg-[#cd3116]/50"
+            }`}
+          >
+            <img
+              src={user.profilePic || assets.avatar_icon}
+              alt="Profile Pic"
+              className="w-[35] aspect-[1/1] rounded-full"
+            />
+            <div className="flex flex-col leading-5">
+              <p>{user.fullName}</p>
+              {index < 3 ? (
+                <span className="bg-green-400 text-xs">Online</span>
+              ) : (
+                <span className="bg-neutral-400 text-xs">Offline</span>
+              )}
+            </div>
+            {index > 2 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {index}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
